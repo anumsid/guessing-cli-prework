b50df823bd0c2dc3df9196e26588909f780a2ead
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-04-17 20:45:36 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -10,8 +10,9 @@ def run_guessing_game
       puts "You guess the correct number!"
     else
       puts "The computer guessed #{number}"
+      input = "exit"
+      puts "Goodbye!"
     end
   end
-  input = "exit"
-  puts "Goodbye!"
+
 end
